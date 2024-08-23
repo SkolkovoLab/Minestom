d6566b33a6f0769ac7628b8c45a652baf1b07ce0
MelonHell <MelonHell@ya.ru>
2024-08-23 18:04:47 +0300

    removed chunk assert lock

--- src/main/java/net/minestom/server/instance/DynamicChunk.java
+++ src/main/java/net/minestom/server/instance/DynamicChunk.java
@@ -341,6 +341,6 @@ public class DynamicChunk extends Chunk {
     }
 
     private void assertLock() {
-        assert Thread.holdsLock(this) : "Chunk must be locked before access";
+//        assert Thread.holdsLock(this) : "Chunk must be locked before access";
     }
 }
